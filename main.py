d242edc05ddbe8ab2def1950d401f79a564307a8
segfaultscribe <segfaultscribe@gmail.com>
2025-09-04 21:08:58 +0530

    added redis based context retrieval

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.products import ProductsRouter
 from dotenv import load_dotenv
+import uuid
  
 app = FastAPI()
 
@@ -19,8 +20,25 @@ app.add_middleware(
 
 load_dotenv()
 
+# middleware to add session cookie to each user, we'll need to rate limit em'
+@app.middleware("http")
+async def add_session_cookie(request: Request, call_next):
+    response: Response = await call_next(request)
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        session_id = str(uuid.uuid4())
+        response.set_cookie(
+            key="session_id",
+            value=session_id,
+            httponly=True,
+            samesite="Lax"
+        )
+    return response
+
+app.include_router(ProductsRouter, prefix="/api/products", tags=["products"])
+
 @app.get("/api")
 async def root():
     return {"message": "Welcome to Product-safety!"}
 
-app.include_router(ProductsRouter, prefix="/api/products", tags=["products"])
+
---
+++ src/helper/context.py
@@ -0,0 +1,19 @@
+import json
+from core.redis_client import redis_client
+
+def save_product_info(session_id: str, role: str, content: str):
+    key = f"chat:{session_id}"
+    history = redis_client.get(key)
+    if history:
+        messages = json.loads(history)
+    else:
+        messages = []
+    messages.append({"role": role, "content": content})
+    redis_client.setex(key, 1800, json.dumps(messages)) # 30min TTL
+
+    return messages
+
+def get_history(session_id: str):
+    key = f"chat:{session_id}"
+    history = redis_client.get(key)
+    return json.loads(history) if history else []
